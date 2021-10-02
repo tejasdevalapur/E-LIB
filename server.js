970dea31c65e2dc0e6c326c1b2fb87c4f9dd21d4
Tejas <devtej2344@gmail.com>
2021-10-02 18:07:23 +0530

    make it better

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-const expressLayouts= require('express-Layouts')
+const expressLayouts= require('express-ejs-layouts')
 const indexRouter = require('./routes/index.js')
 const app= express()
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
+
 app.use(expressLayouts)
 app.use(express.static('public'))
 
